dcf80eed1a00d0d4b3f6729676771a077e403232
thssd <thiagoshimizusodre@gmail.com>
2026-06-27 15:39:29 -0300

    feat: first commit

---
+++ src/test/java/com/challenge/itau/ItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
