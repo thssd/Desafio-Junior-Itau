62be7e5f84b309c8f8e2d6293ec5fdecaa0ed58f
thssd <thiagoshimizusodre@gmail.com>
2026-06-27 19:23:29 -0300

    feat: documentação da api

--- src/main/java/com/challenge/itau/controller/EstatisticaController.java
+++ src/main/java/com/challenge/itau/controller/EstatisticaController.java
@@ -2,6 +2,9 @@ package com.challenge.itau.controller;
 
 import com.challenge.itau.dto.EstatisticaDTO;
 import com.challenge.itau.service.EstatisticasService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,12 @@ public class EstatisticaController {
     private EstatisticasService estatisticasService;
 
     @GetMapping
+    @Operation(description = "endpoint responsavel por buscar estatisticas de transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "transação gravada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "erro de requisição")
+
+    })
     public ResponseEntity<EstatisticaDTO> receberEstatiscas(@RequestParam(value = "intervaloBusca",
             required = false, defaultValue = "60") Integer intervaloBusca) {
         return ResponseEntity.ok(estatisticasService.estatisticaTransacao(intervaloBusca));
--- src/main/java/com/challenge/itau/controller/TransacaoController.java
+++ src/main/java/com/challenge/itau/controller/TransacaoController.java
@@ -2,6 +2,9 @@ package com.challenge.itau.controller;
 
 import com.challenge.itau.dto.TransacaoDTO;
 import com.challenge.itau.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,14 @@ public class TransacaoController {
     private TransacaoService transacaoService;
 
     @PostMapping
+    @Operation(description = "endpoint responsavel por adicionar transaçções")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "campos não atendem os requisitos" +
+                    " da transação"),
+            @ApiResponse(responseCode = "400", description = "erro de requisição")
+
+    })
     public ResponseEntity<Void> criarTransacao(@RequestBody TransacaoDTO dto) {
         transacaoService.adicionarTransacao(dto);
 
@@ -22,6 +33,12 @@ public class TransacaoController {
     }
 
     @DeleteMapping
+    @Operation(description = "endpoint responsavel por deletar transaçções")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "transação deletada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "erro de requisição")
+
+    })
     public ResponseEntity<Void> deletarTransacao() {
         transacaoService.limparTransacao();
 
